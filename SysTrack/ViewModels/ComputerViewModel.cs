4561049fcd94d55e90f08cfb2a46c5132605b6c5
cxdlx <loonyv@outlook.com>
2023-09-28 15:09:20 +0200

    Update

---
+++ SysTrack/ViewModels/ComputerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SysTrack.Models;
+using System.Collections.ObjectModel;
+
+namespace SysTrack.ViewModels
+{
+    public class ComputerViewModel
+    {
+        public ObservableCollection<Computer> Computers { get; set; }
+
+        public ComputerViewModel()
+        {
+            Computers = new ObservableCollection<Computer>();
+            // Hier könnten Sie die Computers-Liste aus einer Datenquelle befüllen
+        }
+
+        // Weitere Methoden und Eigenschaften zur Handhabung der Benutzerinteraktion
+    }
+}
---
+++ SysTrack/Views/ComputerView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SysTrack.Views
+{
+    /// <summary>
+    /// Interaktionslogik für ComputerView.xaml
+    /// </summary>
+    public partial class ComputerView : Page
+    {
+        public ComputerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
