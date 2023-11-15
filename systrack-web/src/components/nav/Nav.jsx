c6210ef782ef1b28596f5386ef3ffae4df8673cf
cxdlx <cxclx@outlook.com>
2023-11-15 10:27:05 +0100

    dashboard

--- systrack-web/src/components/nav/Nav.jsx
+++ systrack-web/src/components/nav/Nav.jsx
@@ -1,8 +1,10 @@
 import styles from './Nav.module.scss'
+import Logo from '../../assets/img/logo.png'
 
 function Nav() {
     return (
         <div className={styles.nav}>
+            <img src={Logo} alt='sysTrack'></img>
             <nav>
 
             </nav>
