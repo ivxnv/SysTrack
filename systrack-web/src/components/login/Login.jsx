9fa1c54f20185d601f711f90a23706656a050d51
Lean <cxclx@outlook.com>
2023-11-16 15:28:58 +0100

    dashboard

--- systrack-web/src/components/login/Login.jsx
+++ systrack-web/src/components/login/Login.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -14,18 +16,47 @@ import styles from "./Login.module.scss";
 library.add(faMugHot, faGear, faUser, faXmark);
 
 function Login() {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
     <div className={styles.login}>
       <div className={styles.container}>
-        <form id="login">
-          <h2>Login</h2>
-          <div className={styles.data__container}>
-            <Input placeholder="Username" />
-            <Input placeholder="Password" type="password" />
-          </div>
-          <Button variant="primary" label="Login" />
-          <span>Create Account</span>
-        </form>
+        {showLogin ? (
+          <form id="login">
+            <div className={styles.data__container}>
+              <h2>Login</h2>
+              <Input placeholder="Email" type="email"/>
+              <Input placeholder="Password" type="password" />
+            </div>
+            <div className={styles.submit}>
+              <Button variant="primary" label="Login" />
+              <span>Do not have an account?</span>
+              <span className={styles.Link} onClick={toggleForm}>
+                Create Account
+              </span>
+            </div>
+          </form>
+        ) : (
+          <form id="signup">
+            <div className={styles.data__container}>
+              <h2>Signup</h2>
+              <Input placeholder="Email" type="email" />
+              <Input placeholder="Password" type="password" />
+              <Input placeholder="Repeat Password" type="password" />
+            </div>
+            <div className={styles.submit}>
+              <Button variant="primary" label="Signup" />
+              <span>Already have an account?</span>
+              <span className={styles.Link} onClick={toggleForm}>
+                Login
+              </span>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
--- systrack-web/src/components/nav/Nav.jsx
+++ systrack-web/src/components/nav/Nav.jsx
@@ -4,10 +4,16 @@ import Logo from '../../assets/img/logo.png'
 function Nav() {
     return (
         <div className={styles.nav}>
+            <div className={styles.items}>
             <img src={Logo} alt='sysTrack'></img>
             <nav>
-
+                <ul>
+                    <li>My Profile</li>
+                    <li>Computers</li>
+                </ul>
             </nav>
+            </div>
+            <span>unknown@unknown</span>
         </div>
     )
 }
